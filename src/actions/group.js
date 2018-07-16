55b6b8a01caa44ef5a61dd447456c9e852585ad4
jaydeaton <jayrdeaton@gmail.com>
2018-07-16 10:25:29 -0500

    update to group

--- src/actions/group.js
+++ src/actions/group.js
@@ -1,12 +1,24 @@
 let cosmetic = require('cosmetic'),
+  { basename, resolve } = require('path'),
   { Group_Shortcut, Group, Shortcut } = require('../emporium'),
-  { printError } = require('../helpers');
+  { abbreviateDirectory, printError } = require('../helpers');
 
 module.exports = async (options) => {
   let { name, replace } = options;
   let { shortcuts } = options._parents.deplace;
 
-  if (shortcuts.length === 0) throw new Error('No shortcuts provided');
+  if (shortcuts.length === 0) {
+    let dir = resolve('.');
+    let shortcut, existingShortcuts = await Shortcut.get({ filter: { dir } });
+    if (existingShortcuts.length > 0) {
+      shortcut = existingShortcuts[0];
+    } else {
+      shortcut = await Shortcut.create({ name: basename(dir), dir});
+      console.log(`${cosmetic.green('Added:')} Shortcut ${cosmetic.cyan(shortcut.name)} for ${cosmetic.cyan(abbreviateDirectory(shortcut.dir))}`);
+    };
+    shortcuts.push(shortcut.name);
+  };
+
   let group, groups = await Group.get({ filter: { name: new RegExp(`^${name}$`, 'i') } });
   if (groups.length > 0) {
     group = groups[0];
